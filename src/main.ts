0c8dc69268efbeddbf145cc62f61a0d55afc3de1
Ocione Carneiro <ocione.junior@ceresinvestimentos.com>
2021-08-10 14:26:07 -0300

    Logging

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform.interceptor';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -17,6 +20,10 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  const port = 3000;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -18,12 +18,15 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
+import { Logger } from '@nestjs/common';
 
 @ApiBearerAuth()
 @Controller('tasks')
 @UseGuards(AuthGuard())
 @ApiTags('Tasks')
 export class TasksController {
+  private logger = new Logger('TasksController');
+
   constructor(private tasksService: TasksService) {}
 
   @Get()
@@ -31,6 +34,11 @@ export class TasksController {
     @Query() filterDto: GetTasksFilterDto,
     @GetUser() user: User,
   ): Promise<Task[]> {
+    this.logger.verbose(
+      `User "${user.username}" retrieving all tasks. Filters: ${JSON.stringify(
+        filterDto,
+      )}`,
+    );
     return this.tasksService.getTasks(filterDto, user);
   }
 
@@ -39,6 +47,11 @@ export class TasksController {
     @Body() task: CreateTaskDto,
     @GetUser() user: User,
   ): Promise<Task> {
+    this.logger.verbose(
+      `User "${user.username}" creating a new task. Data: ${JSON.stringify(
+        task,
+      )}`,
+    );
     return this.tasksService.createTask(task, user);
   }
 
